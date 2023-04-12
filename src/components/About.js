dff2f48ab530e6338174f635b4e63ab7e0e4f069
Katherine Nishimura <katherine.nishimura@live.ca>
2023-04-11 19:35:29 -0700

    adding why section setup

--- src/components/About.js
+++ src/components/About.js
@@ -36,13 +36,13 @@ export default function About() {
           <div id="kat-desc">
             <h4>I am a full stack web developer with a passion for design.</h4>
             <p>
-              I can admit that at first I am not the most extroverted person but
+              I will admit that at first I am not the most extroverted person but
               I love meeting new people and inviting in new perspectives and
               ideas. As much as I love coding and problem solving, I love
               designing websites and choosing theme colors and fonts. I believe
               that design is as important as the functionalities of a website.
               Overcoming problems and finding solutions with my teammates is a
-              feeling that keeps me motivated. When I'm not coding, I'm most
+              something that deeply motivates me. When I'm not coding, I'm most
               likely hiking, birdwatching, or trying new restaurants! I also
               love using my creativity through{" "}
               <a href="https://www.sakuramoonstudio.com/">jewellery making</a>.
@@ -53,43 +53,42 @@ export default function About() {
           <h4>My Story</h4>
           <article>
             <span>
-              <button onClick={() => {winnipegClick()}}>
+              <button classNames={winnipeg ? "city-selected" : ""} onClick={() => {winnipegClick()}}>
                 Winnipeg <FiArrowRight></FiArrowRight>
               </button>
-              <button onClick={() => {torontoClick()}}>
+              <button classNames={toronto ? "city-selected" : ""} onClick={() => {torontoClick()}}>
                 Toronto <FiArrowRight></FiArrowRight>
               </button>
-              <button onClick={() => {vancouverClick()}}>
+              <button classNames={vancouver ? "city-selected" : ""} onClick={() => {vancouverClick()}}>
                 Vancouver <GrLocationPin></GrLocationPin>
               </button>
             </span>
             {winnipeg && (
               <p className="story">
-                I was born in Winnipeg, but grew up in a small city, a few hours
+                I was born in Winnipeg, but grew up in a small city a few hours
                 away. I moved back to Winnipeg in 2015 after recieving a
                 promotion for work. I worked in retail management and then
                 transitioned into restaurant management. I fell in love with the
-                city and it's charms and made some lifelong friends but soon I
-                realized I wanted to have new experiences.
+                city and it's charms, and made some lifelong friends. After a few years I
+                realized I wanted to have new experiences and explore my creative nature.
               </p>
             )}
             {toronto && (
               <p className="story">
-                I wanted to pursue my love for painting and drawing and my
-                desire to be creative. In 2019 decided to move to Toronto to
-                attend OCAD University. Then the pandemic hit, and the education
+                I wanted to pursue my love for painting and drawing so in 2019 decided to move to Toronto to
+                attend OCAD University. When the pandemic hit the education
                 I had longed for became an entirely unexpected experience. I
-                felt very dissatisifed with the path I was on. I started to
+                felt very dissatisifed with the path I was on and I did not want to feel trapped by the "sunken cost fallacy". I started to
                 wonder about other ways I could be creative and decided that I
-                wanted to try Web Development.
+                wanted to pivot into Web Development.
               </p>
             )}
             {vancouver && (
               <p className="story">
-                In 2022 once the pandemic had slowed down I decided to move to Vancouver
-                to be closer to nature and to my sister. I decided to go
-                to Lighthouse Labs to try the bootcamp and see if Web
-                Development was for me. By March 2023 I successfully earned a
+                In 2022 once the pandemic had slowed down I moved to Vancouver
+                to be closer to nature and closer to my sister. I decided to go
+                to Lighthouse Labs to try their bootcamp and see if Web
+                Development was for me. In March 2023 I successfully earned a
                 diploma in Web Development. I loved every step of the way and
                 felt like I finally was on the right path.
               </p>
--- src/components/Why.js
+++ src/components/Why.js
@@ -1,19 +1,25 @@
 import React from "react";
 
-
 export default function Why() {
   return (
-    <section id='why'>
+    <section id="why">
       <div>
-      <h2>Why I Want To Work With You</h2>
-      <p>test 123</p>
+        <h2>Why I Want To Work With You</h2>
+        <div className="why-desc">
+          <article className="why-reasons" >reason 1</article>
+          <article className="why-reasons" >reason 2</article>
+          <article className="why-reasons" > reason 3</article>
+        </div>
       </div>
 
       <div>
-      <h2>Why You Should Hire Me</h2>
-      <p>test 123</p>
+        <h2>Why You Should Hire Me</h2>
+        <div className="why-desc">
+        <article className="why-reasons" >skill 1</article>
+          <article className="why-reasons" >skill 2</article>
+          <article className="why-reasons" >skill 3</article>
+        </div>
       </div>
-
     </section>
-  )
+  );
 }
