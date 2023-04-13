b82a78e2521bfdbdc6b4623b9ebe7ae1cf6ab05f
Katherine Nishimura <katherine.nishimura@live.ca>
2023-04-12 22:42:00 -0700

    responsive nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import Link from "next/link";
 import { FaBars } from 'react-icons/fa'
 
 export default function Nav() {
+
+  const [nav, setNav] = useState(true)
+
   return (
     <nav className='nav-bar'>
     <h1 className='logo-name'>Katherine <span id="last-name">Nishimura</span></h1>
-    <ul className='nav-list'>
+    <ul id="nav-list" className={nav ? 'nav-list' : "hamburger-nav"}>
       <li className='nav-item'><Link href={'#about'}>About Me</Link></li>
       <li className='nav-item'><Link href={'#why'}>Why You and I</Link></li>
       <li className='nav-item'><Link href={'#projects'} scrollSmooth>Projects</Link></li>
@@ -14,7 +17,7 @@ export default function Nav() {
       <li><a className='resume' href="https://flowcv.com/resume/7ll6cs8fn2" target='_blank' rel="noreferrer noopener">Resume</a></li>
     </ul>
     <div className='hidden'>
-      <FaBars />
+      <button id="hamburger" onClick={() => setNav(!nav)}><FaBars /></button>
     </div>
   </nav>
 
